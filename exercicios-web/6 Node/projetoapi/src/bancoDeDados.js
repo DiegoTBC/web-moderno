72ae84ee3855205bda6ed277d02ec28d9cf6ac73
Diego Torres <torressdiiego@outlook.com>
2020-07-08 16:42:32 -0300

    a

---
+++ exercicios-web/6 Node/projetoapi/src/bancoDeDados.js
@@ -0,0 +1,6 @@
+const suquence = {
+    _id: 1,
+    get id() {return this._id++}
+}
+
+const produtos = {}
---
+++ exercicios-web/6 Node/projetoapi/src/servidor.js
@@ -0,0 +1,14 @@
+// cada aplicação usa uma porta
+const porta = 3003
+
+const express = require('express')
+const app = express()
+
+
+app.get('/produtos', (req, res, next) => {
+    res.send({nome: "Notebook", preco: 123.45}) // Convete para JSON
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor executando na porta ${porta}.`)
+})
